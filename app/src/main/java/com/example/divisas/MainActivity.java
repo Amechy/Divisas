f6c4eb6edb4502ba11b42cb82d7f0c40b72e24b6
Alejandro Mechine Gamarra <alejandromechinegamarra@gmail.com>
2017-09-26 12:52:27 +0200

    Sin terminar

---
+++ app/src/main/java/com/example/divisas/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.divisas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    double Euro = 0;
+    double Dolar = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
